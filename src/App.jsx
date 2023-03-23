dc440df88bc1fc4b5cfdedcb413737ae6be67a27
David Diaz <davidcol123dm@gmail.com>
2023-03-23 08:56:04 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  
+
+  return (
+    <div className="App">
+      <h2>Hola mundo</h2>
+    </div>
+  )
+}
+
+export default App
